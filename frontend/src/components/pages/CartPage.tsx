aab70f20aea04443e8c933c0b5f1cd121f1806d6
avivtsadik <48044366+avivtsadik@users.noreply.github.com>
2022-12-09 01:20:53 +0200

    connect to server

--- frontend/src/components/pages/CartPage.tsx
+++ frontend/src/components/pages/CartPage.tsx
@@ -1,41 +1,83 @@
-import React, {useContext, useState} from "react";
-import {ProductType} from "@services/productsManager";
-import {Alert, Grid, Snackbar} from "@mui/material";
-import {ProductCard} from "@components/ProductCard";
-import {CartContext} from "@components/App";
+import React, { useContext, useState } from "react";
+import { ProductType } from "@services/productsManager";
+import { Alert, Grid, Snackbar } from "@mui/material";
+import { ProductCard } from "@components/ProductCard";
+import { CartContext } from "@components/App";
 import Button from "@mui/material/Button";
-import {Map} from 'immutable'
+import { Map } from "immutable";
+import Typography from "@mui/material/Typography";
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+const saveCart = gql`
+  mutation MakeOrder($orderData: OrderData) {
+    makeOrder(orderData: $orderData)
+  }
+`;
 
 const CartPage = () => {
-    const {cartProducts, setCartProducts} = useContext(CartContext);
-    const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const { cartProducts, setCartProducts } = useContext(CartContext);
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
 
-    const onOrderItems = () => {
-        setCartProducts(Map());
-        setSnackBarOpen(true);
-    }
+  const [addItem, { data, loading, error }] = useMutation(saveCart, {
+    onCompleted() {
+      setCartProducts(Map());
+      setSnackBarOpen(true);
+    },
+  });
+  const onOrderItems = () => {
+    addItem({
+      variables: {
+        orderData: {
+          orderedItems: Array.from(cartProducts.keys()),
+          firstName: "Aviv",
+          lastName: "Tsadik",
+        },
+      },
+    });
+  };
 
-    return (
+  return (
+    <>
+      {cartProducts.size != 0 ? (
         <>
-            <Grid container justifyContent={'end'}>
-                <Button variant='contained' sx={{mt: 10}} onClick={onOrderItems}>
-                    Order items
-                </Button>
-            </Grid>
-            <Grid container spacing={6}>
-                {Array.from(cartProducts.values()).map((product: ProductType, key) => (
-                    <Grid key={key} item xl={3} md={4} xs={6}>
-                        <ProductCard product={product} isAddable={false}/>
-                    </Grid>
-                ))}
-            </Grid>
-            <Snackbar open={snackBarOpen} autoHideDuration={3000} onClose={() => setSnackBarOpen(false)}>
-                <Alert onClose={() => setSnackBarOpen(false)} severity="success" sx={{width: '100%'}}>
-                    Ordered!
-                </Alert>
-            </Snackbar>
+          <Grid container justifyContent={"end"}>
+            <Button variant="contained" sx={{ mt: 10 }} onClick={onOrderItems}>
+              Order items
+            </Button>
+          </Grid>
+          <Grid container spacing={6}>
+            {Array.from(cartProducts.values()).map(
+              (product: ProductType, key) => (
+                <Grid key={key} item xl={3} md={4} xs={6}>
+                  <ProductCard product={product} isAddable={false} />
+                </Grid>
+              )
+            )}
+          </Grid>
         </>
-    );
-}
+      ) : (
+        <>
+          <Typography sx={{ mt: 10 }} variant="h4">
+            No products in the shopping cart yet...
+          </Typography>
+        </>
+      )}
+
+      <Snackbar
+        open={snackBarOpen}
+        autoHideDuration={3000}
+        onClose={() => setSnackBarOpen(false)}
+      >
+        <Alert
+          onClose={() => setSnackBarOpen(false)}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Ordered!
+        </Alert>
+      </Snackbar>
+    </>
+  );
+};
 
 export default CartPage;
--- frontend/src/components/pages/HomePage.tsx
+++ frontend/src/components/pages/HomePage.tsx
@@ -1,18 +1,37 @@
-import React, {useState} from "react";
-import {ProductCard} from "@components/ProductCard";
-import {products, ProductType} from "@services/productsManager";
-import {Grid} from "@mui/material";
+import React, { useState } from "react";
+import { ProductCard } from "@components/ProductCard";
+import { ProductType } from "@services/productsManager";
+import { Grid, Typography } from "@mui/material";
+import { gql, useQuery } from "@apollo/client";
+
+const getAllItems = gql`
+  query getAllItems {
+    item {
+      id
+      storeName
+      productName
+      desc
+      price
+      imagePrimary
+    }
+  }
+`;
 
 const HomePage = () => {
-    return (
-        <Grid container sx={{mt: 10}} spacing={6}>
-            {products.map((product: ProductType, key) => (
-                <Grid key={key} item xl={3} md={4} xs={6}>
-                    <ProductCard product={product} isAddable={true}/>
-                </Grid>
-            ))}
-        </Grid>
-    );
+  const { loading, error, data } = useQuery(getAllItems);
+  return (
+    <Grid container sx={{ mt: 10 }} spacing={6}>
+      {!loading ? (
+        data.item.map((product: ProductType, key: any) => (
+          <Grid key={key} item xl={3} md={4} xs={6}>
+            <ProductCard product={product} isAddable={true} />
+          </Grid>
+        ))
+      ) : (
+        <Typography variant="h4">Loading...</Typography>
+      )}
+    </Grid>
+  );
 };
 
 export default HomePage;
